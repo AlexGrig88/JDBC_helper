f8baadd29b8b79422aeaddf36648e068fc422351
Aleksandr Grigorenko <grigxia86@gmail.com>
2022-10-30 11:40:37 +0300

    добавлен метод exampleBatch для отправки сразу нескольких запросов

--- src/com/alexgrig/TransactionRunner.java
+++ src/com/alexgrig/TransactionRunner.java
@@ -4,6 +4,7 @@ package com.alexgrig;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TransactionRunner {
 
@@ -44,6 +45,42 @@ public class TransactionRunner {
             }
             if (deleteTicketsStatement != null) {
                 deleteTicketsStatement.close();
+            }
+        }
+    }
+
+    // Пример работы с отправкой нескольких запросов, упакованных в batch
+    // (имеет смысл для операций delete/update/insert или DDL)
+    public static void exampleBatching() throws SQLException {
+        long flightId = 8;
+        var deleteFlightSql = "DELETE FROM flight WHERE id = " + flightId;
+        var deleteTicketsSql = "DELETE FROM ticket WHERE flight_id = " + flightId;
+        var createTableSql = "CREATE TABLE test4 (id SERIAL PRIMARY KEY);";
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            connection = ConnectionManager.open();
+            connection.setAutoCommit(false);
+
+            statement = connection.createStatement();
+            statement.addBatch(deleteTicketsSql);
+            statement.addBatch(deleteFlightSql);
+            statement.addBatch(createTableSql);
+
+            var ints = statement.executeBatch();
+
+            connection.commit();
+        } catch (Exception e) {
+            if (connection != null) {
+                connection.rollback();
+            }
+            throw e;
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+            if (statement != null) {
+                statement.close();
             }
         }
     }
