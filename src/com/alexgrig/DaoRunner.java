778627e4fec4d8f9336aadc5af48c562aba37b26
Aleksandr Grigorenko <grigxia86@gmail.com>
2022-10-31 22:28:01 +0300

    добавлены методы update, findById, findAll

--- src/com/alexgrig/DaoRunner.java
+++ src/com/alexgrig/DaoRunner.java
@@ -4,12 +4,27 @@ import com.alexgrig.dao.TicketDao;
 import com.alexgrig.entity.Ticket;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 public class DaoRunner {
     public static void main(String[] args) {
 
         //saveTest();
-        deleteTest(56L);
+        //deleteTest(56L);
+        var tickets = TicketDao.getInstance().findAll();
+        tickets.forEach(ticket -> System.out.println(ticket.getPassengerName()));
+
+    }
+
+    private static void updateTest() {
+        var ticketDao = TicketDao.getInstance();
+        var maybeTicket = ticketDao.findById(2L);
+        System.out.println(maybeTicket);
+
+        maybeTicket.ifPresent(ticket -> {
+            ticket.setCost(BigDecimal.valueOf(188.88));
+            ticketDao.update(ticket);
+        });
     }
 
     private static void deleteTest(Long id) {
